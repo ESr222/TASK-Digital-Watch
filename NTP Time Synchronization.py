121273b47ec70767630415b05dc37a769ba1dbbe
ESr222 <147071863+ESr222@users.noreply.github.com>
2023-12-06 00:07:16 +0100

    Create NTP Time Synchronization.py

---
+++ NTP Time Synchronization.py
@@ -0,0 +1,7 @@
+# TASK-Digital-Watch
+
+# Set the NTP server to time.google.com
+ntptime.host = 'time.google.com'
+
+# Adjust for Spanish timezone (UTC+1 or UTC+2 for daylight saving)
+time_offset = 3600  # Change this as needed
