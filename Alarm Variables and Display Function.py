571f25f7522ddf5b45f7be4352b7c000064e4845
ESr222 <147071863+ESr222@users.noreply.github.com>
2023-12-06 00:10:06 +0100

    Create Alarm Variables and Display Function.py

---
+++ Alarm Variables and Display Function.py
@@ -0,0 +1,10 @@
+# TASK-Digital-Watch
+
+# Alarm variables
+alarm_hour = 18
+alarm_minute = 31
+alarm_set = False
+
+def display_binary_time(hour, minute, second):
+    # Function to display time in binary format
+    ...
