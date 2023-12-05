f2705f5e144c35792f8ddae10c770293a6b568ab
ESr222 <147071863+ESr222@users.noreply.github.com>
2023-12-06 00:11:21 +0100

    Create Main Loop.py

---
+++ Main Loop.py
@@ -0,0 +1,13 @@
+# TASK-Digital-Watch
+
+# Sync time immediately after Wi-Fi connection
+sync_time()
+
+# Main loop
+dots = False
+last_sync_time = time.time()
+sync_interval = 600
+
+while True:
+    # Loop to update time, display, check alarm, and resync time
+    ...
